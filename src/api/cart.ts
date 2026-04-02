382a1319247a159a0ad4bb739d89ce4020460308
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-04-02 06:58:57 -0700

    push front for production

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -1,40 +1,24 @@
-import axios from 'axios';
+import api from "./api";
 import type { Cart } from "../types/cart";
 
-const client = axios.create({
-  baseURL: "http://localhost:8000/api",
-  //withCredentials: true,
-});
-
-
-// 🔑 attach token automatically
-client.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
 
 export async function fetchCart() {
-  const res = await client.get<Cart>("/cart/");
+  const res = await api.get<Cart>("/cart/");
   return res.data;
 }
 
 export async function addToCart(product_id: number, quantity = 1) {
-  return client.post("/cart/add/", { product_id, quantity });
+  return api.post("/cart/add/", { product_id, quantity });
 }
 
 export async function updateCartItem(item_id: number, quantity: number) {
-  return client.patch("/cart/update/", { item_id, quantity });
+  return api.patch("/cart/update/", { item_id, quantity });
 }
 
 export async function removeCartItem(product_id: number) {
-  return client.post("/cart/remove/", { product_id });
+  return api.post("/cart/remove/", { product_id });
 }
 
 export async function clearCart() {
-  return client.post("/cart/clear/");
+  return api.post("/cart/clear/");
 }
--- src/api/products.ts
+++ src/api/products.ts
@@ -1,20 +1,16 @@
-import axios from "axios";
+import api from "./api";
 import type { Product, ProductDetail, Category } from "../types/product";
 
-const client = axios.create({baseURL: "http://localhost:8000/api"});
-
 export async function listProducts(params?: { search?: string; category?: string; page?: number }) { 
-  const res = await client.get< Product[] >("/products/", { params });
+  const res = await api.get< Product[] >("/products/", { params });
    return res.data; }
 
-  
-
      export async function  listCategories() {
-      const res = await client.get<Category[]>("/categories/");
+      const res = await api.get<Category[]>("/categories/");
       return res.data;
      }
 
      export async function getProduct(slug: string) {
-       const res = await client.get<ProductDetail>(`/products/${slug}/`);
+       const res = await api.get<ProductDetail>(`/products/${slug}/`);
         return res.data;
      }
