beda0a43d713ee55eb478d523fc602c6bee2266b
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-04-02 06:59:12 -0700

    push front for production

--- src/components/Home/HomeCard.tsx
+++ src/components/Home/HomeCard.tsx
@@ -1,5 +1,6 @@
 import type{ Product } from "../../types/product";
 import { Link } from "react-router-dom";
+import { getImageUrl } from "../../utils/getImageUrl";
 
 type HomeCardProps = {
    product: Product; 
@@ -7,21 +8,20 @@ type HomeCardProps = {
 
 export  function HomeCard({ product }: HomeCardProps) {
      return ( 
-        <div className=" rounded shadow hover:scale-105 bg-white transition">
+        <div className="  rounded shadow hover:scale-105  bg-white transition transform duration-300">
          
             <Link to={`/products/${product.slug}`}> 
-            <img src={ "https://picsum.photos/300"} 
+            <img src={ getImageUrl(product.image)} 
             alt={product.name}
-             className="w-full h-48 object-cover" /> 
+             className="w-full h-48 object-cover rounded-t" /> 
              </Link> 
              <div className="p-2">
                <h3 className="text-lg text-dgray font-semibold">{product.name}</h3> 
                <p className="text-dgray">{product.price}ETB</p>
-                <p className="text-sm text-sky">
+                <p className="text-sm text-burg bg-bird rounded-full px-2 py-1 inline-block ">
                    {product.category?.name}
                </p> 
                </div> 
                </div>
               ); 
             }
-// product.image ?? "/placeholder.png"
