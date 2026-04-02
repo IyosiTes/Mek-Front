65900052d2dd49dbb8756f5ed43ae818e96dbb46
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-04-02 06:59:08 -0700

    push front for production

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -9,15 +9,15 @@ export default function Header() {
       <div className="absolute inset-0 bg-black/40"></div>
 
       <div className="relative z-10 max-w-2xl px-4 flex flex-col items-center gap-6">
-        <h1 className="text-new font-bold text-3xl sm:text-4xl md:text-5xl xl:text-6xl mt-9 2xl:text-7xl">
-          Welcome to Mek
+        <h1 className="text-burg font-bold text-3xl sm:text-4xl md:text-5xl xl:text-6xl mt-9 2xl:text-7xl">
+          Welcome
         </h1>
-        <p className="text-new text-base sm:text-lg md:text-xl xl:text-2xl 2xl:text-3xl mt-6">
+        <p className="text-burg text-base sm:text-lg md:text-xl xl:text-2xl 2xl:text-3xl mt-6">
           The first ecommerce platform for Ethiopian Orthodox goods and services
         </p>
 
         {/* Reusable SearchBar */}
-        <SearchBar placeholder="Search products, vendors, or services..." />
+        <SearchBar />
       </div>
     </header>
     );
