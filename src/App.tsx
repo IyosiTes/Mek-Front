3bc1fe1e3bb19a1ba7203a11423f8f0858382e46
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-04-02 06:58:39 -0700

    push front for production

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layouts/Layout";
-//import  navBarLinks   from "./components/ui/NavBarLinks";
 import { HomePage } from "./components/Pages/HomePage";
 import  NotFoundPage  from "./components/Pages/NotFoundPage";
 import  ProductDetailPage  from "./components/Pages/ProductDetailPage";
 import CartPage  from "./components/Pages/CartPage";
 import LoginPage from "./components/Pages/LoginPage";
 //import ProtectedRoute from "./components/Auth/ProtectedRoute";
+import RegisterPage from "./components/Pages/RegisterPage";
+import CheckoutPage from "./components/Pages/CheckoutPage";
+import OrderSuccessPage from "./components/Pages/OrderSuccess";
+import UserProfile from "./components/Pages/UserProfile";
+import AuthLayout from "./components/Layouts/AuthLayout";
+import OrdersPage from "./components/Pages/OrdersPage";
+import ForgotPasswordPage from "./components/Pages/ForgotPasswordPage";
+import ResetPasswordPage from "./components/Pages/ResetPasswordPage";
+import PrivacyPolicy from "./components/Pages/PrivacyPolicy";
+import TermsOfService from "./components/Pages/TermOfService";
+import RefundPolicy from "./components/Pages/RefundPolicy";
+import ShippingPolicy from "./components/Pages/ShippingPolicy";
 
 
-/*
-import ProductDetailPage from "./pages/ProductsPage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
-*/
+
 
 
 
@@ -22,18 +28,25 @@ export default function App() {
   return (
     
        <Routes>
+          <Route element={<AuthLayout />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/Forgot-Password" element={<ForgotPasswordPage />}/>
+        <Route path="/Reset-password" element={<ResetPasswordPage />} />
+         <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route path="/terms" element={<TermsOfService />} />
+            <Route path="/refund" element={<RefundPolicy />} />
+            <Route path="/shipping" element={<ShippingPolicy />} />
+          </Route>
         <Route path='/' element={<Layout />}>
        <Route index element={<HomePage />} />  
-        <Route path="/Login" element={<LoginPage />} />
-       { /*
-          <Route path="/products/:slug" element={<ProductDetailPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />*/}
-          
+           <Route path ="/profile" element={<UserProfile/>}/>
           <Route path ="/cart" element={<CartPage />} />
           <Route path="/products/:slug" element={<ProductDetailPage />} />
-           <Route path="*" element={<NotFoundPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/order-success/:id" element={<OrderSuccessPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+            <Route path="/orders" element={<OrdersPage />} />
         </Route>
      </Routes>
     
