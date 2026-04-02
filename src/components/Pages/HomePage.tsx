bb430b2dea5bcf57e3d0ca85d45fdbf009291779
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-04-02 07:00:26 -0700

    push front for production

--- src/components/Pages/HomePage.tsx
+++ src/components/Pages/HomePage.tsx
@@ -1,23 +1,16 @@
-
 import Header from "../Home/Header";
 import { CardContainer } from "../Home/CardContainer";
 import { useProducts } from "../../hooks/useProducts";
 import { ProductSkeletonGrid } from "../ui/ProductSGrid";
 import { ErrorMessage } from "../ui/ErrorMessage";
 
-
-
 export  function HomePage() {
   const { data, isLoading, error } = useProducts();
 
-
-
   return (
-    
     <div>
     <Header />
     <div >
-     
       <h1 className="justify-self-center text-2xl font-bold mb-6 text-dgray">
         Our Products
         </h1>
