246981b1c380d739f25c099733d042e6577f2f65
Eyosias Tesfaye <tesfayeyosias01@gmail.com>
2026-07-09 02:12:31 -0700

    feat: Rebuilding full community layer

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,30 +2,18 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
+
 });
 
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("access");
 
-    const publicRoutes = [
-      "/auth/login/",
-      "/auth/register/",
-      "/auth/forgot-password/",
-      "/auth/reset-password/",
-    ];
+    if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    const isPublic = publicRoutes.some((route) =>
-      config.url?.startsWith(route)
-    );
-
-    if (token && config.headers && !isPublic) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  return config;
+});
 
 export default api;
